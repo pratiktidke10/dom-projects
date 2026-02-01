693ad612fb8379f8ecea5e74d04e68e4a6b7a9ae
Pratik Tidke <pratiktidke987@gmail.com>
2026-02-01 16:04:36 +0530

    javascript added

--- 1-colorChanger/script.js
+++ 1-colorChanger/script.js
@@ -1,4 +1,10 @@
 //change color on clicking button
 
+const button = document.querySelectorAll('.button');
+const body = document.querySelector('body');
 
-
+button.forEach((btn) => {
+    btn.addEventListener('click' , () => {
+        document.body.style.backgroundColor = btn.id;
+    });
+});
