e66d48bb0d840a9ac445194aeae5ea3f2bae8b60
Pratik Tidke <pratiktidke987@gmail.com>
2026-02-04 16:04:46 +0530

    keyboard javascript added

--- 5-keyboard/script.js
+++ 5-keyboard/script.js
@@ -1,1 +1,22 @@
-console.log('Project 5');
+
+const insert = document.querySelector('#insert');
+
+window.addEventListener('keydown' , (e) => {
+    insert.innerHTML = `
+    <div class = 'color'>
+    <table>
+    <tr>
+        <th>key</th>
+        <th>keyCode</th>
+        <th>Code</th>
+    </tr>
+    <tr>
+        <td>${e.key}</td>
+        <td>${e.keyCode}</td>
+        <td>${e.code}</td>
+    </tr>
+    </table>
+
+    </div>
+    `
+})
